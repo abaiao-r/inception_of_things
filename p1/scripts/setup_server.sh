5817e59cc9a1b6a3a349f22513b8a9282e5c57cb
IcQuackson <pedrorenato1998@gmail.com>
2026-09-01 18:42:53 +0100

    feat: add k3 server provision

---
+++ p1/scripts/setup_server.sh
@@ -0,0 +1,6 @@
+export DEBIAN_FRONTEND=noninteractive	# prevents access to stdin that give errors
+
+sudo apt-get update -y
+sudo apt-get install curl -y
+curl -sfL https://get.k3s.io | sh -		# register k3s server
+cp /var/lib/rancher/k3s/server/node-token /vagrant/k3s-node-token		# moves token to shared folder so agent can access later
